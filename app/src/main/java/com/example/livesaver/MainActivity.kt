ecb3602228ed285c9bc32f22b72e422d7eccf96b
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-07-03 09:08:45 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/livesaver/MainActivity.kt
+++ app/src/main/java/com/example/livesaver/MainActivity.kt
@@ -22,11 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
         lifecycleScope.launch {
             appEntryUsecases.readAppEntry().collect{
                 Log.d("aht", "onCreate: $it")
